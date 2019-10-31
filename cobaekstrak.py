e228f649e3198d2c194beb9c6e9949a337de955b
Yahya1547 <1547yahya@gmail.com>
2019-10-31 23:11:06 +0700

    Update cobaekstrak.py

--- cobaekstrak.py
+++ cobaekstrak.py
@@ -6,50 +6,44 @@ import pickle
 import random
 import os
 import matplotlib.pyplot as plt
-from fungsi import *
 
-image_path = r"PINS"
+image_path = r"PINS/pins_Aaron Paul"
 image_dir = os.listdir(image_path)
 
 def extract(images_path):
-    for files in os.listdir(image_path):
-        for files2 in os.listdir(os.path.join(image_path,files)):
-            image = imread(files2, mode="RGB")
+    image = imread(images_path)
             
-            alg = cv2.KAZE_create()
+    alg = cv2.KAZE_create()
                 # Dinding image keypoints
-            kps = alg.detect(image)
+    kps = alg.detect(image)
                 # Getting first 32 of them. 
                 # Number of keypoints is varies depend on image size and color pallet
                 # Sorting them based on keypoint response value(bigger is better)
-            kps = sorted(kps, key=lambda x: -x.response)[:32]
+    kps = sorted(kps, key=lambda x: -x.response)[:32]
                 # computing descriptors vector
-            kps, dsc = alg.compute(image, kps)
+    kps, dsc = alg.compute(image, kps)
                 # Flatten all of them in one big vector - our feature vector
-            dsc = dsc.flatten()
+    dsc = dsc.flatten()
                 # Making descriptor of same size
                 # Descriptor vector size is 64
-            needed_size = (32 * 64)
-            if dsc.size < needed_size:
+    needed_size = (32 * 64)
+    if dsc.size < needed_size:
                     # if we have less the 32 descriptors then just adding zeros at the
                     # end of our feature vector
-                dsc = np.concatenate([dsc, np.zeros(needed_size - dsc.size)])
+        dsc = np.concatenate([dsc, np.zeros(needed_size - dsc.size)])
 
-            return dsc
+    return dsc
             
 def batch_extractor(images_path, pickled_db_path="features.pck"):
     files_batch = [os.path.join(images_path, p) for p in sorted(os.listdir(images_path))]
-
+    print(files_batch)
     result = {}
-    for i in files_batch:
-        for f in os.listdir(i):
-            print ('Extracting features from image %s' % f)
-            name = f.split('/')[-1].lower()
-            result[name] = extract(f)
-        
+    for f in files_batch:
+        print ('Extracting features from image %s' % f)
+        tmp=os.path.join(files_batch,f)
+        print(extract(tmp))
+    
     # saving all our feature vectors in pickled file
-    with open(pickled_db_path, 'w') as fp:
-        pickle.dump(result, fp)
 
 
 
@@ -101,19 +95,8 @@ def run():
     # getting 3 random images 
     sample = random.sample(files, 3)
     
-    batch_extractor(images_path)
+    for i in files:
+        print(extract(i))
 
-    ma = Matcher('features.pck')
-    
-    for s in sample:
-        print ('Query image ==========================================')
-        show_img(s)
-        names, match = ma.match(s, topn=3)
-        print ('Result images ========================================')
-        for i in range(3):
-            # we got cosine distance, less cosine distance between vectors
-            # more they similar, thus we subtruct it from 1 to get match value
-            print ('Match %s') % (1-match[i])
-            show_img(os.path.join(images_path, names[i]))
 
 run()
